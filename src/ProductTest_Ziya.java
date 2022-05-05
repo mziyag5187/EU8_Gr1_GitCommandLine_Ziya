2da3114b60126e466610cb0c694540a0e4805f90
mziya <mziyag5187@gmail.com>
2022-05-05 18:07:07 +0300

    Product ACs is completed

--- src/ProductTest_Ziya.java
+++ src/ProductTest_Ziya.java
@@ -1,5 +1,6 @@
 public class ProductTest_Ziya {
     public static void main(String[] args) {
         System.out.println("Product class is created");
+        System.out.println("Product AC1 is PASSED");
     }
 }
