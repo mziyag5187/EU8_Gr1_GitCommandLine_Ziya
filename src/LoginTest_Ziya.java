af8ee479505b06ce4bb3bb832cf7d64494e73ef1
mziya <mziyag5187@gmail.com>
2022-05-05 17:43:16 +0300

    AC1 is completed

--- src/LoginTest_Ziya.java
+++ src/LoginTest_Ziya.java
@@ -1,5 +1,6 @@
 public class LoginTest_Ziya {
     public static void main(String[] args) {
         System.out.println("Login class is created");
+        System.out.println("AC1 testing is PASSED");
     }
 }
