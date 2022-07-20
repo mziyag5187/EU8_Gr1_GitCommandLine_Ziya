b7aabd936e8daaa3cb6e872b40e5d57cdafecee7
mziya <62723491+mziyag5187@users.noreply.github.com>
2022-07-20 21:26:02 +0300

    Update ProductTest_Ziya.java

--- src/ProductTest_Ziya.java
+++ src/ProductTest_Ziya.java
@@ -2,5 +2,7 @@ public class ProductTest_Ziya {
     public static void main(String[] args) {
         System.out.println("Product class is created");
         System.out.println("Product AC1 is PASSED");
+        System.out.println("Product AC2 is PASSED");
+        System.out.println("Product AC3 is PASSED");        
     }
 }
